5d193db8ccd1036381c8cad39d7225f391544e32
Asim Waheed <a7waheed@uwaterloo.ca>
2026-01-02 20:29:35 -0500

    Move all constants to separate file

---
+++ src/constants/site.ts
@@ -0,0 +1,12 @@
+// src/constants/site.ts
+
+export const SITE = {
+  name: "Asim Waheed",
+  homeHref: "/",
+
+  email: "asim.waheed29@gmail.com",
+
+  github: "https://www.github.com/asim29",
+  scholar: "https://scholar.google.com/citations?user=PgPuHyQAAAAJ",
+  linkedin: "https://www.linkedin.com/in/asim-waheed97/",
+} as const;
